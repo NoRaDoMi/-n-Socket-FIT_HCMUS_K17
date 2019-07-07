79f22841b2c2dfcefa96166d88d42b9dfab4e113
PhucVo <fakermsi1999@gmail.com>
2019-07-07 12:46:05 +0700

    Final Source. Done!!!

---
+++ DoAnSocket/src/PROXY_SERVER/HamBoTro.h
@@ -0,0 +1,315 @@
+﻿#pragma once
+#pragma once
+
+#include "PROXY_SERVER.h"
+#include<string>
+#include<iostream>
+#include <fstream>
+#include <mutex>
+#include<vector>
+
+
+using namespace std;
+
+#define EXPIRED 180  // Thời gian tồn tại của 1 request cache trong CACHE
+#define NTH	100
+
+string filecachepath = "Cache/"; // Thư mục chứa cache
+
+
+typedef struct _ParsedRequest
+{
+	string method; // GET or POST
+	string path;
+	string host;
+	string range;
+	string port;
+}ParsedRequest;
+
+
+typedef struct reqInfor {
+	string filename;
+	time_t timeout;
+};
+
+
+
+vector<reqInfor> CACHE;
+
+void deleCache(int i) {
+	remove(CACHE[i].filename.c_str());
+	CACHE.erase(CACHE.begin() + i);
+} // Hàm xóa 1 cache request trong CACHE
+
+bool isEXPIRED(int i, time_t now) {
+	return (time(&CACHE[i].timeout) - time(&now)) <= EXPIRED;
+} // Hàm kiểm tra cache còn hạn không ? -- 3 phút
+
+bool checkCahe(string filename, time_t now) {
+	for (int i = 0; i < CACHE.size(); i++) {
+		if (CACHE[i].filename == filename) {
+			if (isEXPIRED(i, now)) // kiểm tra request cache còn hạn sử dụng không ?
+				return true;
+			else {
+				deleCache(i); // neu no ton tai nhung thoi gian EXPIRED lon hon thoi gian cho phep thi xoa 
+				return false;
+			}
+		}
+	}
+	return false;// neu ko ton tai thi return false
+} // Hàm kiểm tra request có thể sử dụng trong CACHE không ?
+
+string toFilename(string host, string path) {
+	string t1;
+	string t2;
+	if (host.size() < 30)
+	{
+		t1 = host;
+	}
+	else
+		t1 = host.substr(0, 30);
+
+	if (path.size() < 30)
+	{
+		t2 = path;
+	}
+	else
+		t2 = path.substr(path.size() - 30, 30);
+	string res = "";
+
+	string temp = t1 + t2;
+	int i;
+
+	// Xóa các kí tự không dùng cho đặt tên file trong Window
+	while ((i = temp.find("<", 0)) != string::npos) {
+		temp.replace(i, 1, "_");
+	}
+
+	while ((i = temp.find(">", 0)) != string::npos) {
+		temp.replace(i, 1, "_");
+	}
+
+	while ((i = temp.find(":", 0)) != string::npos) {
+		temp.replace(i, 1, "_");
+	}
+
+	while ((i = temp.find("\"", 0)) != string::npos) {
+		temp.replace(i, 1, "_");
+	}
+
+	while ((i = temp.find("/", 0)) != string::npos) {
+		temp.replace(i, 1, "_");
+	}
+	while ((i = temp.find("\\", 0)) != string::npos) {
+		temp.replace(i, 1, "_");
+	}
+
+	while ((i = temp.find("|", 0)) != string::npos) {
+		temp.replace(i, 1, "_");
+	}
+
+	while ((i = temp.find("*", 0)) != string::npos) {
+		temp.replace(i, 1, "_");
+	}
+
+	while ((i = temp.find("?", 0)) != string::npos) {
+		temp.replace(i, 1, "_");
+	}
+	res = res + temp + ".txt";
+	return res;
+} // Hàm tạo file name cho file chứa response cho 1 request từ client (host,path)
+
+bool isInBlackList(string host) {
+	ifstream blacklistFile;
+	string line;
+
+	size_t pos;
+
+	blacklistFile.open("blacklist.conf");
+
+	if (blacklistFile.fail()) {
+		std::cout << "fail to open blacklist.conf" << endl;
+		return false;
+	}
+	else
+	{
+		//std::cout << "\nopen success" << endl;
+		while (getline(blacklistFile, line))
+		{
+			pos = line.find(host);
+			if (pos != string::npos) {
+				return true;
+			}
+		}
+	}
+	return false;
+} // Hàm kiểm tra 1 Http Request có nằm trong blacklist của PROXY không ?
+
+char *get_ip(const char *host)
+{
+	struct hostent *hent;
+	struct in_addr** addr_list;
+
+	//memset(hent, 0, sizeof(hent));
+	int iplen = 15; //XXX.XXX.XXX.XXX
+	char *ip = (char *)malloc(iplen + 1);
+	memset(ip, 0, iplen + 1);
+	if ((hent = gethostbyname(host)) == NULL)
+	{
+		printf("Can't get IP\n");
+		return NULL;
+	}
+
+	addr_list = (struct in_addr**)hent->h_addr_list;
+	if (addr_list[0] == NULL)
+		return NULL;
+	strcpy(ip, inet_ntoa(*addr_list[0]));
+	return ip;
+} // Hàm lấy IP từ 1 host name trong Http request
+
+  //Ref: http://stackoverflow.com/questions/19715144/how-to-convert-char-to-lpcwstr
+wchar_t *convertCharArrayToLPCWSTR(const char* charArray)
+{
+	wchar_t* wString = new wchar_t[4096];
+	MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, 4096);
+	return wString;
+}
+
+void Build_HTTP_Requests(string &req) {
+
+	int start = req.find("http://", 0);
+	if (start == string::npos)
+		return;
+
+	int end = req.find("/", start + 7);
+	req = req.erase(start, end - start);
+
+	/*start = req.find("Accept-Encoding: ", 0);
+	if (start == string::npos)
+	return;
+
+	end = req.find("\r\n", start) + 2;
+	req = req.erase(start, end - start);*/
+
+	start = req.find("Connection: ", 0);
+	if (start == string::npos)
+		return;
+
+	end = req.find("\r\n", start);
+	req = req.erase(start + 12, end - start - 12);
+	req = req.insert(start + 12, "close");
+	//return PORT;
+} // Hàm build 1 Http Request để gửi lên Remote Server
+
+
+bool parse_HttpRequest(string* client_request, ParsedRequest* req) {
+
+	int startPos = -1;
+	int endPos = -1;
+	endPos = client_request->find(" ht", 0);
+
+	if (endPos == string::npos)
+	{
+		return false;
+	}
+
+	startPos = 0;
+
+	char *request_method = new char[endPos + 2];
+	memset(request_method, 0, endPos + 2);
+	memcpy(request_method, (char *)(*client_request).c_str(), endPos);
+
+	req->method = request_method; // KIỂU REQUEST <> GET OR POST : BSS...
+
+	startPos = client_request->find("://", endPos) + 3;
+	endPos = client_request->find("/", startPos);
+
+
+	char *request_host = new char[endPos - startPos + 2];
+	memset(request_host, 0, endPos - startPos + 2);
+	memcpy(request_host, (char *)(*client_request).c_str() + startPos, endPos - startPos);
+
+	req->host = request_host; // HOST của REQUEST
+
+	startPos = endPos;
+	endPos = client_request->find(" HTTP/1", startPos);
+	char *request_path = new char[endPos - startPos + 2];
+	memset(request_path, 0, endPos - startPos + 2);
+	memcpy(request_path, (char *)(*client_request).c_str() + startPos, endPos - startPos);
+
+	req->path = request_path; // PATH của REQUEST
+
+	startPos = client_request->find("Range: ", endPos);
+	if (startPos == string::npos) // Nếu trong request không có RANGE.
+	{
+		delete[]request_host;
+		delete[]request_method;
+		delete[]request_path;
+		return true;
+	}
+	// NÊU CÓ
+	startPos += 7;
+	endPos = client_request->find("\r\n", startPos);
+	char * request_range = new char[endPos - startPos + 2];
+	memset(request_range, 0, endPos - startPos + 2);
+	memcpy(request_range, (char *)(*client_request).c_str() + startPos, endPos - startPos);
+
+	req->range = request_range;
+
+	delete[]request_host;
+	delete[]request_method;
+	delete[]request_path;
+	return true;
+} // Hàm parse 1 Http request 
+
+void loadingAni() {
+	cout << "  Launching  ";
+	int i = 0;
+	while (i < 3) {
+		cout << ".";
+		Sleep(500);
+		i++;
+	}
+	cout << endl << "\a Ready to use !" << endl;
+}
+
+string buildHttpRequest(ParsedRequest req) {
+	string m_RequestHeader;
+
+	m_RequestHeader = m_RequestHeader + req.method + " " + req.path + " HTTP/1.1\r\n";
+	m_RequestHeader = m_RequestHeader + "Host: " + req.host + "\r\n";
+	m_RequestHeader = m_RequestHeader + "Connection: keep-alive\r\n";
+	m_RequestHeader = m_RequestHeader + "User-Agent: Novasoft NetPlayer/4.0\r\n";
+	//	m_RequestHeader=m_RequestHeader+"Cache-Control: max-age=0\r\n";
+	m_RequestHeader = m_RequestHeader + "Accept: */*\r\n";
+	//	m_RequestHeader=m_RequestHeader+"Origin:  http://222.73.105.196\r\n";
+	//	m_RequestHeader=m_RequestHeader+"Cookie: saeut=61.188.187.53.1323685584721318\r\n";
+	if (!req.range.empty())
+	{
+		m_RequestHeader = m_RequestHeader + "Range: " + req.range + "\r\n";
+	}
+	m_RequestHeader += "\r\n";
+
+	return m_RequestHeader;
+}
+
+
+long getHttpResponseSize(string *m_ResponseHeader) {
+	long nFileSize = 0;
+	char szValue[10];
+	int nPos = -1;
+	nPos = m_ResponseHeader->find("Content-Length", 0);
+	if (nPos != -1)
+	{
+		nPos += 16;
+		int nCr = m_ResponseHeader->find("\r\n", nPos);
+		memcpy(szValue, (char *)m_ResponseHeader->c_str() + nPos, nCr - nPos);
+		nFileSize = atoi(szValue);
+		return nFileSize;
+	}
+	else
+	{
+		std::cout << "Khong the lay do dai Response tu may chu\r\n";
+		return -1;
+	}
+}
---
+++ DoAnSocket/src/PROXY_SERVER/PROXY_SERVER.cpp
@@ -0,0 +1,332 @@
+﻿// Proxy_Server_Final.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include "afxsock.h"
+#include"403_FORBIDDENRES.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+#define BUFFSIZ 9192
+
+// The one and only application object
+
+CWinApp theApp;
+
+
+mutex mtx; // Sử dụng cho chặn luồng
+
+
+bool isForbidden = 0;
+
+
+
+
+/////////////// HÀM CHẠY LUỒNG XỬ LÍ CÁC REQUEST TỪ CLIENT //////////////////
+DWORD WINAPI HandleThread(LPVOID arg)
+{
+	SOCKET* hConnected = (SOCKET*)arg;
+	CSocket p_client;
+	p_client.Attach(*hConnected);
+
+	char c;
+	int iResult;
+	int totalLen = 0;
+	std::string browser_request;
+	char buffer[BUFFSIZ + 1] = { 0 };
+
+	////////////////////// NHẬN HTTP REQUEST TỪ CLIENT //////////////////////////////
+	do {
+		iResult = p_client.Receive(buffer, BUFFSIZ, 0);
+		totalLen += iResult;
+		if (iResult <= 0) {
+			//cout << "Thoat ujd" << endl;
+			break;
+		}
+		buffer[iResult] = '\0';
+		browser_request.append(buffer, iResult);
+		if (browser_request.find("\r\n\r\n") != string::npos) {
+			break;
+		}
+		memset(buffer, 0, BUFFSIZ + 1);
+	} while (1);
+	/////////////////////////////////////////////////////////////////////////////////
+
+
+
+	std::cout << "Request: " << std::endl;
+	std::cout << browser_request << endl;
+	/*std::cout << "Do dai nhan: " << totalLen << std::endl;
+	std::cout << "Do dai chua: " << browser_request.size() << std::endl << std::endl;*/
+
+
+
+	ParsedRequest Parsed_Request; // Struct chứa Parsed Http Request
+
+
+								  // Parse request nhận từ browser
+	if (!parse_HttpRequest(&browser_request, &Parsed_Request)) {
+		//cout << "Failed" << endl;
+		return 0;
+	} //Nếu parse không thành công
+
+	  // Build lại 1 Http Request từ Client
+	  // Chỉnh sửa: 
+	  //           + Connection = close
+	  //			 + Bỏ tên host trong URI-request
+	Build_HTTP_Requests(browser_request);
+
+
+	/*std::cout << "Request sau khi sua: " << std::endl;
+	std::cout << browser_request << endl;
+	std::cout << "Do dai sau khi sua: " << browser_request.size() << std::endl << std::endl;*/
+
+	//////////////////  PROXY CHỈ HỖ TRỢ GET VÀ POST METHOD //////////////////
+	if (Parsed_Request.method != "GET" && Parsed_Request.method != "POST") {
+		//Chỉ xử lý GET hoặc POST
+		p_client.Close();
+		return 0;
+	}
+
+
+
+	//////////////// KIỂM TRA PAGE CÓ NẰM TRONG BLACKLIST //////////////////////
+	if (isForbidden == 1 || isInBlackList(Parsed_Request.host)) {
+		if (isForbidden == 0)
+			isForbidden = 1;
+		std::cout << "Ngan truy cap" << endl;
+		p_client.Send(forbiddenResponse.c_str(), forbiddenResponse.size(), 0);
+
+		p_client.Close();
+		return 0;
+	}
+
+
+
+	// Tạo file name cho request
+	string filename = filecachepath + toFilename(Parsed_Request.host, Parsed_Request.path);
+
+	// KIEM TRA REQUEST TRONG CACHE
+	time_t now = time(0); // Lấy thời gian hiện tại của hệ thống
+	CSocket p_Server; // Socket kết nối đến server và nhận về response
+	FILE * f;
+
+	///////////////////// KIỂM TRA PROXY CACHING TRƯỚC KHI GỬI REQUEST LÊN REMOTE SERVER /////////////////////////////
+
+	if (checkCahe(filename, now)) { //	Nếu có trong CACHE
+
+		mtx.lock(); // Chặn các luông khác
+		fopen_s(&f, filename.c_str(), "rb");
+		if (f != nullptr) {
+			cout << "REQUEST co trong CACHE CHO :" << filename << " va con HAN DUNG !" << endl;
+			char buffer_cache[1025] = { 0 };
+			while (1) {
+				int len = fread_s(buffer_cache, 1025, 1, 1024, f);
+				if (len == 0)
+					break;
+				p_client.Send(buffer_cache, len, 0);
+				memset(buffer_cache, 0, 1025);
+			}
+			fclose(f);
+			mtx.unlock();
+			p_client.Close();
+			return 0;
+		}
+	}
+	else { // Nếu không thì gửi Http request vừa build lên Remote Server
+
+		p_Server.Create(0, SOCK_STREAM); // Tạo Socket TCP
+
+		char* ip = get_ip(Parsed_Request.host.c_str()); // Lấy địa chỉ IP từ host name request
+
+		///////////// KẾT NỐI ĐẾN REMOTE SERVER //////////////////////
+		if (p_Server.Connect(convertCharArrayToLPCWSTR(ip), 80) < 0)
+		{
+			std::cout << "Cannot connect to Server !" << endl;
+			p_Server.Close();
+			p_client.Close();
+			return 0;
+		}
+
+
+		// Mảng chứa Http Response từ Remote server
+		char * browser_req = new char[browser_request.size() + 1]; // browser_request : string nên cần chuyển sang mảng char
+		char * temp = browser_req;
+
+		memcpy_s(browser_req, browser_request.size() + 1, browser_request.c_str(), browser_request.size());
+		totalLen = 0;
+
+		//////////////// GỬI HTTP REQUEST LÊN REMOTE SERVER /////////////////////
+		while (totalLen < browser_request.size()) {
+			iResult = p_Server.Send(temp + totalLen, browser_request.size(), 0);
+			totalLen += iResult;
+			if (iResult < 0) break;
+		}
+		delete[]browser_req;
+
+		//////////////// NHẬN HTTP RESPONSE TỪ REMOTE SERVER /////////////////////
+		int totalSent = 0;
+		int iSent;
+		totalLen = 0;
+		memset(buffer, 0, BUFFSIZ + 1);
+		string HTTPResponse = "";
+
+		do {
+			iResult = p_Server.Receive(buffer, BUFFSIZ, 0);
+
+			cout << "Do dai nhan " << iResult << endl;
+			totalLen += iResult;
+			if (iResult == 0) {
+				break;
+			}
+			if (iResult < 0) {
+				//std::cout << "Thoat kkk" << endl;
+				break;
+			}
+			buffer[iResult] = '\0';
+			iSent = 0;
+			int t;
+			while (iSent < iResult) {
+				t = p_client.Send(buffer, iResult, 0);
+				//cout << "Do dai response gui ve browser " << t << endl;
+				if (t == 0) break;
+				if (t < 0) {
+
+					//std::cout << "Thoat abk" << endl;
+					break;
+				}
+
+				iSent += t;
+			}
+			totalSent += iSent;
+
+			HTTPResponse.append(buffer, iResult);
+			memset(buffer, 0, BUFFSIZ + 1);
+
+		} while (1);
+
+
+		/////////////////// GHI NHẬN HTTP RESPONSE VÀO FILE ĐẺ LÀM CACHE CHO CÁC REQUEST TIẾP THEO /////////////////////
+		mtx.lock();
+		f = NULL;
+		////Kiểm tra đã có luồng nào viết file chưa
+		//if (fopen_s(&f, filename.c_str(), "rb") == 0) {
+		//	fclose(f);
+		//}
+
+		fopen_s(&f, filename.c_str(), "wb");
+		if (f != NULL) {
+			fwrite(HTTPResponse.c_str(), 1, HTTPResponse.size(), f);
+
+			// push vào CACHE
+			reqInfor p;
+			p.filename = filename;
+			time_t now = time(0);
+			p.timeout = now;
+
+			CACHE.push_back(p);
+			fclose(f);
+		}
+		else {
+			cout << "Failed on " << filename << endl;
+		}
+
+		mtx.unlock();
+
+	}
+
+	p_client.Close();
+	p_Server.Close();
+
+	std::cout << "KET THUC ...\r\n";
+	return 0;
+}
+
+
+
+//////////////// HÀM CHẠY LUỒNG CHÍNH QUẢN LÍ CÁC LUỒNG CON XỬ LÍ MULTIREQUEST TỪ CLIENT ////////////////////////
+DWORD WINAPI MainThread(LPVOID)
+{
+	AfxSocketInit(NULL);
+	CSocket proxy;
+	int PORT = 8888;
+	proxy.Create(PORT, SOCK_STREAM);
+
+	cout << "\n  \xB2\xB2\xB2\xB2\xB2\xB2\xB2 OUR PROXY SERVER";
+	cout << "\n  \xB2\xB2\xB2\xB2\xB2\xB2\xB2 IP :127.0.0.1 ";
+	cout << "\n  \xB2\xB2\xB2\xB2\xB2\xB2\xB2 PORT: 8888\r\n";
+	loadingAni();
+
+	if (!proxy.Listen(SOMAXCONN)) {
+		cout << "Cannot listen on this PORT !" << endl;
+	}
+
+	while (1)
+	{
+		CSocket p_client;
+		DWORD threadID;
+		HANDLE threadStatus;
+		if (!proxy.Accept(p_client))
+		{
+			cout << "Ket noi khong thanh cong \r\n";
+			continue;
+		}
+		/*cout << "\n \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\r\n";*/
+
+		SOCKET* hConnected = new SOCKET();
+		*hConnected = p_client.Detach();
+
+		//Tao Thread
+		threadStatus = CreateThread(NULL, 0, HandleThread, (LPVOID)hConnected, 0, &threadID);
+		//_beginthread(HandleThread, 0, hConnected);
+
+		cout << "SIZE cua cache: " << CACHE.size() << endl;
+
+		// KIỂM TRA PAGE BỊ CẤM
+		if (isForbidden == 1)
+			isForbidden = 0;
+	}
+
+
+	return 0;
+}
+
+int main()
+{
+	int nRetCode = 0;
+
+	HMODULE hModule = ::GetModuleHandle(nullptr);
+
+	if (hModule != nullptr)
+	{
+		// initialize MFC and print and error on failure
+		if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+		{
+			// TODO: change error code to suit your needs
+			wprintf(L"Fatal Error: MFC initialization failed\n");
+			nRetCode = 1;
+		}
+		else
+		{
+			// TODO: code your application's behavior here.
+			DWORD ThreadID;
+			HANDLE ThreadStatus;
+			ThreadStatus = CreateThread(NULL, 0, MainThread, NULL, 0, &ThreadID);
+			// Để duy trì Proxy server 
+			while (1)
+			{
+				Sleep(10000000);
+			}
+
+		}
+	}
+	else
+	{
+		// TODO: change error code to suit your needs
+		wprintf(L"Fatal Error: GetModuleHandle failed\n");
+		nRetCode = 1;
+	}
+
+	return nRetCode;
+}
